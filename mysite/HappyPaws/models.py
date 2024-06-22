1dbcca21f401d087cd6e80ae5e9dba85880ef5fc
Javier Montecinos <ja.montecinosr@duocuc.cl>
2024-06-22 18:17:54 -0400

    migrate

--- mysite/HappyPaws/models.py
+++ mysite/HappyPaws/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(min_length=7,max_length=20)
+
+class Registro (models.Model):
+        Usuario
