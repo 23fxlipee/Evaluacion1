c2fd13a365c046d82b8d57d684cc973d9c8a12a5
Felipe Arce <fe.arces@duocuc.cl>
2024-06-24 21:40:37 -0400

    Autenticación de usuario

---
+++ mysite/HappyPaws/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("inicio", views.index, name="inicio"),
+    path("productos/perro", views.get_perros, name="perros"),
+    path("productos/gato", views.get_gatos, name="gatos"),
+    path("registro", views.get_registro, name="registro"),
+    path("pago", views.get_pago, name="pago"),
+    path("cuenta/<int:registro_id>", views.get_cuenta, name="cuenta"),
+    path("cuenta/eliminar/<int:registro_id>/", views.delete_registro, name="borrar_registro"),
+    path("cuenta/admin", views.get_lista_cuentas,name="cuentas")
+
+]
