4931381349982852a44124d257a59beb1a9f01bf
Felipe Arce <fe.arces@duocuc.cl>
2024-07-17 02:51:32 -0400

    models

--- mysite/HappyPaws/models.py
+++ mysite/HappyPaws/models.py
@@ -11,4 +11,28 @@ class Registro (models.Model):
         confirmar_pass= models.CharField(max_length=20)
         aceptar_terminos=models.CharField(max_length=20)
 
+class DatosPago(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    numero = models.CharField(max_length=20)
+    codigo_postal = models.CharField(max_length=7)
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    activado = models.BooleanField(default=True)
+
+class DetalleCompra(models.Model):
+    cantidad = models.IntegerField()
+    subtotal = models.FloatField()
+    IVA = models.FloatField()
+    total = models.FloatField() 
+
+class Pedido(models.Model):
+    fecha = models.DateTimeField() 
 
